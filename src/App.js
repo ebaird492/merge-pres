84274ac76167a0a43f1f3afcfbbc7cdb24ae09e3
ebaird492 <ebaird492@gmail.com>
2021-04-14 12:34:13 -0600

    Published

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import merge from './merge-ex.png';
+import example from './merge-sort.png';
+import code from './merge.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h>Merge Sort ...</h>
+        <img src={merge} className="App-logo" alt="logo" />
       </header>
+      <div className="Text">
+        <p>
+          Merge sort divides the array into smaller components
+        </p>
+        <p>
+          Isn't it easier to sort a two element array rather than one of a much larger size?
+        </p>
+        <img src={example} className="Example" alt="example" />
+        <img src={code} className="Code" alt="code example" />
+      </div>
     </div>
   );
 }
